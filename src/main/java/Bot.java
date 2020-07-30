8fbc0fc3f04f65d88b7cdb34265a118a2e44f36a
adrianromanski <adrianromanski@icloud.com>
2020-07-30 21:58:50 +0200

    Initial commit

---
+++ src/main/java/Bot.java
@@ -0,0 +1,13 @@
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
+import javax.security.auth.login.LoginException;
+
+public class Bot {
+
+    public static void main(String[] args) throws LoginException {
+
+        JDA jda = JDABuilder.createDefault("NzM4NDgyNzA3ODE4MDg2NDk0.XyMjnQ.u8Vlg2p62AudXhh4j8L8WwNJGPM").build();
+
+    }
+}
