606debc2bab12c5da55f4629542647cb20e6b1a7
adrianromanski <adrianromanski@icloud.com>
2020-07-30 22:34:46 +0200

    Securing Bot Tokens in PropertiesFile

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -2,12 +2,13 @@ import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 
 import javax.security.auth.login.LoginException;
+import java.io.IOException;
 
 public class Bot {
 
-    public static void main(String[] args) throws LoginException {
+    public static void main(String[] args) throws LoginException, IOException {
 
-        JDA jda = JDABuilder.createDefault("NzM4NDgyNzA3ODE4MDg2NDk0.XyMjnQ.u8Vlg2p62AudXhh4j8L8WwNJGPM").build();
+        JDA jda = JDABuilder.createDefault(PropertiesLoader.getToken()).build();
 
     }
 }
---
+++ src/main/java/PropertiesLoader.java
@@ -0,0 +1,36 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class PropertiesLoader {
+
+    private static InputStream inputStream;
+    private static String result;
+
+
+    static String getToken() throws IOException {
+
+        try {
+            Properties prop = new Properties();
+            String propFileName = "passwords.properties";
+
+            inputStream = PropertiesLoader.class.getClassLoader().getResourceAsStream(propFileName);
+
+            if (inputStream != null) {
+                prop.load(inputStream);
+            } else {
+                throw new FileNotFoundException("property file '" + propFileName + "' not found in the classpath");
+            }
+
+            return prop.getProperty("discord.token");
+
+        } catch (Exception e) {
+            System.out.println("Exception: " + e);
+        } finally {
+            assert inputStream != null;
+            inputStream.close();
+        }
+        return result;
+    }
+}
